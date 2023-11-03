e8d6e59295bca1853aac930924115768207edd16
Adem Ottoman <50057725+Ademrobert@users.noreply.github.com>
2023-11-03 22:07:10 +0200

    Update BioData.js

--- data/BioData.js
+++ data/BioData.js
@@ -1,21 +1,17 @@
 // date: 1 Aug, 2022
 const bioData = [
     {
-        name: 'nxt lnk',
+        name: 'Adem Ottoman',
         username: 'nxtlnk.vercel.app',
         url: 'https://nxtlnk.vercel.app',
         titleImg: true,
         avatar: '/avatar.png',
         nftAvatar: true,
-        description: 'Custom bio links for creatives who love coding. Made this first to use for my own weblinks vjy.me/lnk. ',
+        description: 'Hello! I am Adem a young entrepreneur/Project Manager & other things :DD',
         descShow: true,
-        subdesc: 'Use this template and deploy on vercel for free',
         subdescShow: true,
         newProductUrl: 'https://3dicons.co',
         newProduct: true,
-        author: 'realvjy',
-        authorURL: 'https://twitter.com/realvjy',
-        footerText: 'made by'
     },
 ];
 
